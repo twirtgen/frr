27a74b97dcbc7d7235f89637024bb30750ddbaa2
twirtgen <thomas.wirtgen@uclouvain.be>
2022-05-05 18:19:41 +0200

    Add warning socket

--- bgpd/bgp_path_validation_ping.c
+++ bgpd/bgp_path_validation_ping.c
@@ -67,6 +67,7 @@ int send_ping(struct sockaddr_in *ping_addr, unsigned int timeout_us,
 	// socket()
 	ping_sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
 	if (ping_sockfd < 0) {
+		perror("socket SOCK_RAW IPPROTO_ICMP");
 		return -1;
 	}
 
